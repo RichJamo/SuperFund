dd8e0a7d3e9c3ca1102b349e81a53fc50cccb16b
Richard Jamieson <jamieson.rich@gmail.com>
2024-08-07 13:03:37 +0200

    Create Connect button

---
+++ components/login.tsx
@@ -0,0 +1,20 @@
+import { ConnectButton } from 'thirdweb/react';
+import { client, chain } from '../utils/constants';
+
+const Login: React.FC = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100vh',
+      }}
+    >
+      <ConnectButton client={client} chain={chain} />
+    </div>
+  );
+};
+
+export default Login;
