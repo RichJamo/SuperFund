ef8560130e71aad3503cc30c60e66406ed89dade
Richard Jamieson <jamieson.rich@gmail.com>
2024-08-07 12:28:49 +0200

    Switch to using thirdweb

--- src/utils/apollo-client.ts
+++
@@ -1,9 +0,0 @@
-// src/apollo-client.ts
-import { ApolloClient, InMemoryCache } from '@apollo/client';
-
-const client = new ApolloClient({
-  uri: 'https://api.goldsky.com/api/public/project_cl94kmyjc05xp0ixtdmoahbtu/subgraphs/superform-v1-10/1.1.2/gn',
-  cache: new InMemoryCache(),
-});
-
-export default client;
